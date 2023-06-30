0c2e4be514bf35e9704ee2727d445c9cd1310a16
Mauro <mauromaio.dev@gmail.com>
2023-06-30 13:43:06 -0300

    footer-component-changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import Image from "next/image";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="flexStart footer">
+      <div className="flex flex-col gap-12 w-full">
+        <div className="flex items-start flex-col">
+          <Image
+            src="/logo-purple.svg"
+            width={115}
+            height={38}
+            alt="Flexibble"
+          />
+
+          <p className="text-start text-sm font-normal mt-5 max-w-xs">
+            Poste seus projetos aqui!
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
