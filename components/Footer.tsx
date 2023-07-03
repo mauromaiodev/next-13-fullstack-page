6e3ae30018f481bc973c2426e2e96e26f24c74b0
Mauro <mauromaio.dev@gmail.com>
2023-07-03 20:39:20 -0300

    refactoring-image-component-and-footerColumn-map

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,21 @@
-import Image from "next/image";
+import { footerLinks } from "@/constants";
+import Link from "next/link";
 
-const FooterColumn = () => (
+type ColumnProps = {
+  title: string;
+  links: Array<string>;
+};
+
+const FooterColumn = ({ title, links }: ColumnProps) => (
   <div className="footer_column">
-    <h4 className="font-semibold">Titulo</h4>
-    <ul className="flex flex-col gap-2 font-normal">Links</ul>
+    <h4 className="font-semibold">{title}</h4>
+    <ul className="flex flex-col gap-2 font-normal">
+      {links.map((link) => (
+        <Link href="/" key={link}>
+          {link}
+        </Link>
+      ))}
+    </ul>
   </div>
 );
 
@@ -12,19 +24,17 @@ const Footer = () => {
     <footer className="flexStart footer">
       <div className="flex flex-col gap-12 w-full">
         <div className="flex items-start flex-col">
-          <Image
-            src="/logo-purple.svg"
-            width={115}
-            height={38}
-            alt="Flexibble"
-          />
-
+          {/* <Image src="" width={115} height={38} alt="LOGO" /> */}
+          LOGO
           <p className="text-start text-sm font-normal mt-5 max-w-xs">
             Poste seus projetos aqui!
           </p>
         </div>
         <div className="flex flex-wrap gap-12">
-          <FooterColumn />
+          <FooterColumn
+            title={footerLinks[0].title}
+            links={footerLinks[0].links}
+          />
         </div>
       </div>
     </footer>
