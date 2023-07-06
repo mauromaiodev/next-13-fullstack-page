5c2e4e4c7fed7d64d6b51a25acf25e4be834bd29
Mauro <mauromaio.dev@gmail.com>
2023-07-06 16:10:22 -0300

    adicionado os demais user models

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -2,6 +2,11 @@ import { config, g } from "@grafbase/sdk";
 
 const User = g.model("User", {
   name: g.string().length({ min: 2, max: 20 }),
+  email: g.string().unique(),
+  avatarUrl: g.url(),
+  description: g.string().length({ min: 2, max: 1000 }).optional(),
+  githubUrl: g.url().optional(),
+  linkedinUrl: g.url().optional(),
 });
 
 export default config({
