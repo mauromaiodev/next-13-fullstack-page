c8eddaa4dc86849d05e8d370cf00d376ad2f0643
Mauro <mauromaio.dev@gmail.com>
2023-07-07 12:44:39 -0300

    fetchin-authProviders

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
+import { getProviders } from "next-auth/react";
+
 type Provider = {
   id: string;
   name: string;
@@ -12,7 +16,29 @@ type Provider = {
 type Providers = Record<string, Provider>;
 
 const AuthProviders = () => {
-  return <div>AuthProviders</div>;
+  const [providers, setProviders] = useState<Providers | null>(null);
+
+  useEffect(() => {
+    const fetchProviders = async () => {
+      const res = await getProviders();
+
+      console.log(res);
+
+      setProviders(res);
+    };
+
+    fetchProviders();
+  }, []);
+
+  if (providers) {
+    return (
+      <div>
+        {Object.values(providers).map((provider: Provider, i) => (
+          <button key={i}>{provider.id}</button>
+        ))}
+      </div>
+    );
+  }
 };
 
 export default AuthProviders;
