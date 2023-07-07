a0e7e06db23cabf58d60978c0dcd4f41d56cc1fd
Mauro <mauromaio.dev@gmail.com>
2023-07-07 12:37:25 -0300

    refactoring-authProviders-to-connect-with-grafbase

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,4 +1,15 @@
-import React from "react";
+"use client";
+
+type Provider = {
+  id: string;
+  name: string;
+  type: string;
+  signinUrl: string;
+  callbackUrl: string;
+  signinUrlParams?: Record<string, string> | undefined;
+};
+
+type Providers = Record<string, Provider>;
 
 const AuthProviders = () => {
   return <div>AuthProviders</div>;
